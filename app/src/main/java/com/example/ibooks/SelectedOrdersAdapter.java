226502d7810161c91e3f932eaa3ae6a907cfa414
sam wadi <samwadi97@gmail.com>
2024-02-03 14:43:41 -0600

    fifth commit

--- app/src/main/java/com/example/ibooks/SelectedOrdersAdapter.java
+++ app/src/main/java/com/example/ibooks/SelectedOrdersAdapter.java
@@ -1,15 +1,2 @@
-package com.example.ibooks;
-
-import java.util.List;
-
-public class SelectedOrdersAdapter {
-    private List<Order> orderList;
-    private DeliveryOrderAdapter.OnItemClickListener listener;
-
-    public SelectedOrdersAdapter(List<Order> orderList, DeliveryOrderAdapter.OnItemClickListener listener) {
-        this.orderList = orderList;
-        this.listener = listener;
-    }
-
-
+package com.example.ibooks;public class SelectedOrdersAdapter {
 }
